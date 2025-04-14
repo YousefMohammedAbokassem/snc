d686a563be04878f7b357f1fc140eba66687fd1b
Master Coders <mastercoders@Masters-MacBook-Air.local>
2025-04-15 00:29:45 +0300

    upload

--- vite.config.js
+++ vite.config.js
@@ -1,59 +1,127 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import { VitePWA } from 'vite-plugin-pwa'
+
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     VitePWA({
+//       registerType: 'autoUpdate',
+//       includeAssets: [
+//         'favicon.svg',
+//         'favicon.ico',
+//         'robots.txt',
+//         'apple-touch-icon.png',
+//         'offline.html'
+//       ],
+//       manifest: {
+//         name: 'snc',
+//         short_name: 'snc',
+//         description: 'snc progressive web app',
+//         theme_color: '#ffffff',
+//         background_color: '#ffffff',
+//         display: 'standalone',
+//         start_url: '/',
+//         icons: [
+//           {
+//             src: 'android-chrome-192x192.png',
+//             sizes: '192x192',
+//             type: 'image/png'
+//           },
+//           {
+//             src: 'android-chrome-512x512.png',
+//             sizes: '512x512',
+//             type: 'image/png'
+//           },
+//           {
+//             src: 'android-chrome-512x512.png',
+//             sizes: '512x512',
+//             type: 'image/png',
+//             purpose: 'any maskable'
+//           }
+//         ]
+//       },
+//       workbox: {
+//         runtimeCaching: [
+//           {
+//             urlPattern: ({ request }) => request.mode === 'navigate',
+//             handler: 'NetworkFirst',
+//             options: {
+//               cacheName: 'pages-cache',
+//               networkTimeoutSeconds: 3,
+//               fallbackURL: '/offline.html'
+//             }
+//           }
+//         ]
+//       }
+//     })
+//   ]
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
-export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: [
-        'favicon.svg',
-        'favicon.ico',
-        'robots.txt',
-        'apple-touch-icon.png',
-        'offline.html'
-      ],
-      manifest: {
-        name: 'snc',
-        short_name: 'snc',
-        description: 'snc progressive web app',
-        theme_color: '#ffffff',
-        background_color: '#ffffff',
-        display: 'standalone',
-        start_url: '/',
-        icons: [
-          {
-            src: 'android-chrome-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'android-chrome-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          },
-          {
-            src: 'android-chrome-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable'
-          }
-        ]
+
+const manifestForPlugin = {
+  registerType: "prompt",
+  includeAssets: ['favicon.ico', "apple-touc-icon.png", "masked-icon.png" ],
+  manifest: {
+    name: "Weather app",
+    short_name: "Weather app",
+    description: "An app that can show the weather forecast for your city.",
+    icons: [
+      
+      {
+        src: "./icon-192x192.png",
+        sizes: "192x192",
+        type: "image/png"
       },
-      workbox: {
-        runtimeCaching: [
-          {
-            urlPattern: ({ request }) => request.mode === 'navigate',
-            handler: 'NetworkFirst',
-            options: {
-              cacheName: 'pages-cache',
-              networkTimeoutSeconds: 3,
-              fallbackURL: '/offline.html'
-            }
-          }
-        ]
+      {
+        src: "./icon-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose:'favicon'
+      },
+      {
+        src: '/apple-touch-icon.png',
+        sizes:'180x180',
+        type:'image/png',
+        purpose:'apple touch icon',
+      },
+      {
+        src: "./icon-144x144.png",
+        sizes: "144x144",
+        type: "image/png",
+        purpose: "any"
+      },
+      {
+        src: "./icon-256x256.png",
+        sizes: "256x256",
+        type: "image/png",
+        purpose: "icon"
+      },
+      {
+        src: "./icon-384x384.png",
+        sizes: "384x384",
+        type: "image/png",
+        purpose: "any maskable"
       }
-    })
-  ]
-})
+    ],
+    theme_color: "#181818",
+    background_color: "#e8eac2",
+    display: "standalone",
+    scope: "/",
+    start_url: "/",
+    orientation: "portrait",
+  },
+};
+
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  
+  plugins: [react(), VitePWA(manifestForPlugin)],
+});
