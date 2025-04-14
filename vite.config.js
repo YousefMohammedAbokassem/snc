94049486d0b73c5770678aeb7eabcc7cb41995ed
Master Coders <mastercoders@Masters-MacBook-Air.local>
2025-04-14 23:48:10 +0300

    pwa

--- vite.config.js
+++ vite.config.js
@@ -1,37 +1,3 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
-
-export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
-      manifest: {
-        name: 'اسم التطبيق',
-        short_name: 'اسم قصير',
-        description: 'وصف التطبيق',
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
-    })
-  ]
-})
-
-
-// 
 // import { defineConfig } from 'vite'
 // import react from '@vitejs/plugin-react'
 // import { VitePWA } from 'vite-plugin-pwa'
@@ -41,15 +7,9 @@ export default defineConfig({
 //     react(),
 //     VitePWA({
 //       registerType: 'autoUpdate',
-//       includeAssets: [
-//         'favicon.svg',
-//         'favicon.ico',
-//         'robots.txt',
-//         'apple-touch-icon.png',
-//         'offline.html' // 👈 نضيفها هنا
-//       ],
+//       includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
 //       manifest: {
-//         name: 'اسم تطبيقك',
+//         name: 'اسم التطبيق',
 //         short_name: 'اسم قصير',
 //         description: 'وصف التطبيق',
 //         theme_color: '#ffffff',
@@ -65,20 +25,60 @@ export default defineConfig({
 //             type: 'image/png'
 //           }
 //         ]
-//       },
-//       workbox: {
-//         runtimeCaching: [
-//           {
-//             urlPattern: ({ request }) => request.mode === 'navigate',
-//             handler: 'NetworkFirst',
-//             options: {
-//               cacheName: 'pages-cache',
-//               networkTimeoutSeconds: 3,
-//               fallbackURL: '/offline.html'
-//             }
-//           }
-//         ]
 //       }
 //     })
 //   ]
 // })
+
+
+// 
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: [
+        'snc.png',
+        'snc.png',
+        'snc.png',
+        'snc.png',
+        'offline.html' // 👈 نضيفها هنا
+      ],
+      manifest: {
+        name: 'snc',
+        short_name: 'snc',
+        description: 'snc',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'snc.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'snc.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      },
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: ({ request }) => request.mode === 'navigate',
+            handler: 'NetworkFirst',
+            options: {
+              cacheName: 'pages-cache',
+              networkTimeoutSeconds: 3,
+              fallbackURL: '/offline.html'
+            }
+          }
+        ]
+      }
+    })
+  ]
+})
