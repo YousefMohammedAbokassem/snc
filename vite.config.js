585da0e15ef647b40998cf60f274f7a85b0a45c5
Master Coders <mastercoders@Masters-MacBook-Air.local>
2025-04-15 00:53:12 +0300

    a

--- vite.config.js
+++ vite.config.js
@@ -58,56 +58,53 @@
 //   ]
 // })
 
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 const manifestForPlugin = {
   registerType: "prompt",
-  includeAssets: ['favicon.ico', "apple-touc-icon.png", "masked-icon.png" ],
+  includeAssets: ["favicon.ico", "apple-touc-icon.png", "masked-icon.png"],
   manifest: {
     name: "Weather app",
     short_name: "Weather app",
     description: "An app that can show the weather forecast for your city.",
     icons: [
-      
       {
         src: "./icon-192x192.png",
         sizes: "192x192",
-        type: "image/png"
+        type: "image/png",
       },
       {
         src: "./icon-512x512.png",
         sizes: "512x512",
         type: "image/png",
-        purpose:'favicon'
+        purpose: "favicon",
       },
       {
-        src: '/apple-touch-icon.png',
-        sizes:'180x180',
-        type:'image/png',
-        purpose:'apple touch icon',
+        src: "/apple-touch-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+        purpose: "apple touch icon",
       },
       {
         src: "./icon-144x144.png",
         sizes: "144x144",
         type: "image/png",
-        purpose: "any"
+        purpose: "any",
       },
       {
         src: "./icon-256x256.png",
         sizes: "256x256",
         type: "image/png",
-        purpose: "icon"
+        purpose: "icon",
       },
       {
         src: "./icon-384x384.png",
         sizes: "384x384",
         type: "image/png",
-        purpose: "any maskable"
-      }
+        purpose: "any maskable",
+      },
     ],
     theme_color: "#181818",
     background_color: "#e8eac2",
@@ -118,10 +115,7 @@ const manifestForPlugin = {
   },
 };
 
-
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  
   plugins: [react(), VitePWA(manifestForPlugin)],
 });
