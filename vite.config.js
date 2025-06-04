7d42f9abf67397d153649660274ffca65b9b1791
Master Coders <mastercoders@Masters-MacBook-Air.local>
2025-06-04 11:54:36 +0300

    a

--- vite.config.js
+++ vite.config.js
@@ -76,7 +76,8 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA(manifestForPlugin)],
+  // plugins: [react(), VitePWA(manifestForPlugin)],
+  plugins: [react(), VitePWA()],
 
   // إعدادات جديدة مضافة لدعم Firebase و top-level await
   // build: {
